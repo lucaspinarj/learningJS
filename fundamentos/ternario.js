ff9f8aeb7f1e8c5dca06b1fa01ad8ab22689ce43
lucaspinarj <lucaspinarj@hotmail.com>
2022-07-11 18:26:00 -0300

    updated

---
+++ fundamentos/ternario.js
@@ -0,0 +1,9 @@
+const resultado = nota => nota >= 7 ? 'aprovado' : 'reprovado'
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+function recup(valor) {
+    return nota = valor <= 4 ? 'recuperacao' : 'vasco'
+}
+
+console.log(recup(4.1))
---
+++ fundamentos/unarios.js
@@ -0,0 +1,23 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
+
+let exp1 = Math.pow(2, 3)
+console.log(exp1)
+
+let exp2 = 2 ** 3
+console.log(exp2)
+
+let base = 2
+base **= 3
+console.log(base)
+
+let exp3 = 2 ** -3
+console.log(exp3)
